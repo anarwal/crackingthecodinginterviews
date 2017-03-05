d0a743cc6b9aff79363c3c1c6acabe7005d88704
abhimanyunarwal <anarwal.27@gmail.com>
2017-03-04 18:49:18 -0700

    Chapter-10, problem6 : fixed typo

--- chapter10/src/problem6/Solution.java
+++ chapter10/src/problem6/Solution.java
@@ -7,7 +7,7 @@ package problem6;
 public class Solution {
 
       /*
-      As the question asks just for explaination, I would go ahead and explain my approach. DUe to large size of file
+      As the question asks just for explanation, I would go ahead and explain my approach. DUe to large size of file
       it is not advisable to use traditional method of sorting to read file and sort because when we will start reading
       RAM will run out-of-memory due to large size of file. Therefore we will read file in small chunks and sort it. We
       will read a specific amount of data from file, sort it and then store it in a temp file. We do this for the whole
