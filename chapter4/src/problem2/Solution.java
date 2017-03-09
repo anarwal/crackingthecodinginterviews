80d32970f263b577794bf686ccc9a74e7875527c
abhimanyunarwal <anarwal.27@gmail.com>
2017-03-08 23:03:40 -0700

    Chapter-4, problem2 : fixed typo

--- chapter4/src/problem2/Solution.java
+++ chapter4/src/problem2/Solution.java
@@ -27,7 +27,7 @@ public class Solution {
 /*
 in order for binary tree have minimal height it must be a complete binary tree, it would help binary
 search tree to maintain minimal height.
-Now as array is sorted it is easy for us to choose so we can divide in smaller and greater terms then root
+Now as array is sorted it is easy for us to choose so we can divide in smaller and greater terms then
 choose the middle element of array to be the root, the element on the left will be smaller and element to
 the right will be greater then root, now using recursion and choosing middle, put all the elements in graph
 */
