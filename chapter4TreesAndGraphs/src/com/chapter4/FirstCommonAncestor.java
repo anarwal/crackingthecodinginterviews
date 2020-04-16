8d1a940d14fe08962a14ebb73e7694e787c7b37a
Abhimanyu Narwal <anarwal.27@gmail.com>
2020-04-15 20:50:36 -0400

    chapter 4, problem 8 solution

--- chapter4TreesAndGraphs/src/com/chapter4/FirstCommonAncestor.java
+++ chapter4TreesAndGraphs/src/com/chapter4/FirstCommonAncestor.java
@@ -6,4 +6,29 @@ package com.chapter4;
  */
 
 public class FirstCommonAncestor {
+    public class Node{
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    Node LCA( Node root, Node n1, Node n2){
+        if(root == null) return null;
+
+        if( root == n1 || root == n2) return root;
+
+        Node left = LCA(root.left, n1, n2);
+        Node right = LCA(root.right, n1, n2);
+
+        if(left !=null && right !=null) return root;
+        if(left == null && right == null) return null;
+
+        return left !=null ? left:right;
+    }
 }
