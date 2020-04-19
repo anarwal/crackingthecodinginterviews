e59c3ba0942aa98c97d448d7dd0bfd3bba1eddef
Abhimanyu Narwal <anarwal.27@gmail.com>
2020-04-19 13:04:14 -0400

    chapter 5, algorithms

---
+++ chapter10Sorting/src/com/chapter5/BinarySearchRecursive.java
@@ -0,0 +1,13 @@
+package com.chapter5;
+
+class BinarySearchRecursive{
+	public static int binarySearch(int[] nums, int left, int right, int target){
+        if(right >=left){
+            int mid = (left+right)/2;
+            if(target == nums[mid]) return mid;
+            if(target<nums[mid]) return binarySearch(nums,left, mid-1, target);
+            return binarySearch(nums,mid+1, right, target);
+        }
+        else return -1;
+    }
+}
