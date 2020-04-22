8da5b1b559263029c43e759841dbf3bf03109980
Abhimanyu Narwal <anarwal.27@gmail.com>
2020-04-21 23:01:09 -0400

    updating method

--- chapter1Arrays/src/com.chapter1/ZeroMatrix.java
+++ chapter1Arrays/src/com.chapter1/ZeroMatrix.java
@@ -8,65 +8,69 @@ import java.util.Scanner;
  */
 
 public class ZeroMatrix {
-
-    static void setZeroMatrix(int[][] array){
+    public static void setZeroes(int[][] matrix) {
         boolean rowHasZero = false;
-        boolean columnHasZero =false;
+        boolean columnHasZero = false;
+        //checking first column
+        for(int i=0; i<matrix[0].length; i++){
+            if(matrix[0][i]==0) {
+                rowHasZero=true;
+                break;
+            }
+
+        }
 
         //checking first row
-        for(int i =0; i<array[0].length; i++){
-            if(array[0][i] == 0){
-                rowHasZero = true;
+        for(int j=0; j<matrix.length;j++){
+            if(matrix[j][0]==0) {
+                columnHasZero=true;
                 break;
             }
+
         }
 
-        //checking first column
-        for(int j =0; j<array.length; j++){
-            if(array[j][0] == 0){
-                columnHasZero = true;
-                break;
-            }
-        }
-
-        // checking through rest of matrix
-        for(int i =1; i<array.length; i++){
-            for(int j =1; j<array[0].length; j++){
-                if(array[i][j] == 0){
-                    array[i][0] = 0;
-                    array[j][0] = 0;
+        //checking rest of matrix
+        for(int i=1; i<matrix.length;i++){
+            for(int j=1; j<matrix.length; j++){
+                if(matrix[i][j]==0){
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
                 }
+
             }
         }
 
-        // nullify rows
-        for(int i=1; i < array[0].length; i++){
-            if(array[0][i] == 0){
-                nullify(array, i);
-            }
+        //setting columns zero
+        for(int i=1; i<matrix[0].length; i++){
+            if(matrix[0][i]==0)
+                nullifyColumn(matrix, i);
         }
 
-        // nullify columns
-        for(int j=1; j < array.length; j++){
-            if(array[j][0] == 0){
-                nullify(array, j);
-            }
+        //setting rows zero
+        for(int j=1; j<matrix.length;j++){
+            if(matrix[j][0]==0)
+                nullifyRow(matrix, j);
         }
 
-        // nullify first zero
-        if(rowHasZero){
-            nullify(array, 0);
+        if(columnHasZero) {
+            nullifyColumn(matrix, 0);
         }
 
-        // nullify first column
-        if(columnHasZero){
-            nullify(array, 0);
+        if(rowHasZero) {
+            nullifyRow(matrix, 0);
         }
-
     }
 
-    static void nullify(int[][] array, int n){
-        //method to make given row/column in array zero
+    static void nullifyColumn(int[][] array, int n){
+        for(int i=0; i<array.length; i++){
+            array[i][n]=0;
+        }
+    }
+
+    static void nullifyRow(int[][] array, int n){
+        for(int i=0; i<array[n].length; i++){
+            array[n][i]=0;
+        }
     }
 
     public static void main(String[] args) {
@@ -80,7 +84,7 @@ public class ZeroMatrix {
                 arr[i][j] =scanner.nextInt();
             }
         }
-        setZeroMatrix(arr);
+        setZeroes(arr);
         scanner.close();
     }
 }
