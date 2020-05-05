de70a5c74905383d25bb0895eaf9d00be0a449e9
Abhimanyu Narwal <anarwal.27@gmail.com>
2020-05-05 09:52:59 -0400

    adding more solutions

---
+++ leetcode/src/bloomberg/linkedList/MergeSortedList.java
@@ -0,0 +1,53 @@
+package bloomberg.linkedList;
+
+public class MergeSortedList {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+
+        ListNode node = new ListNode(-1);
+        ListNode cur = node;
+
+        while(l1 != null && l2 !=null ){
+            if(l1.val<=l2.val){
+                cur.next = l1;
+                l1 = l1.next;
+            }else{
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 == null ? l2 : l1;
+        return node.next;
+    }
+
+
+    public ListNode mergeTwoListsRecursive(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        else if (l2 == null) {
+            return l1;
+        }
+        else if (l1.val < l2.val) {
+            l1.next = mergeTwoListsRecursive(l1.next, l2);
+            return l1;
+        }
+        else {
+            l2.next = mergeTwoListsRecursive(l1, l2.next);
+            return l2;
+        }
+    }
+}
